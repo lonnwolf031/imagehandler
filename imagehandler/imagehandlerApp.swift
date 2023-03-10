08aa6ee42638db8999c710e31b55d7a9d5822724
lonnwolf031 <76941636+lonnwolf031@users.noreply.github.com>
2023-03-10 16:08:27 +0100

    Initial Commit

---
+++ imagehandler/imagehandlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  imagehandlerApp.swift
+//  imagehandler
+//
+//  Created by Lonneke Kolkman on 10/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct imagehandlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
