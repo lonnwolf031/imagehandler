4f81a0f1b84600b2c2fd74b8d0c77b1f42199025
lonnwolf031 <76941636+lonnwolf031@users.noreply.github.com>
2023-03-11 16:49:21 +0100

    ~

---
+++ imagehandler/Coordinator.swift
@@ -0,0 +1,24 @@
+//
+//  Coordinator.swift
+//  imagehandler
+//
+//  Created by Lonneke Kolkman on 11/03/2023.
+//
+
+import UIKit
+import SwiftUI
+
+class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    var picker: ImagePickerView
+    
+    init(picker: ImagePickerView) {
+        self.picker = picker
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[.originalImage] as? UIImage else { return }
+        self.picker.selectedImage = selectedImage
+        self.picker.isPresented.wrappedValue.dismiss()
+    }
+    
+}
